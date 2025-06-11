cc0e74dabfca69ac5b5538cb23243dce64217acd
openhouseon <openhouseon@gmail.com>
2025-06-11 16:21:25 -0700

    Update testrepoleaf.c

--- testrepoleaf.c
+++ testrepoleaf.c
@@ -9,4 +9,5 @@ int main()
     printf("Bye GitHub!\n");
     printf("Took out *repoleaf* in .gitignore!\n");
     printf("How do you like those apples. & GitHub asks.\n");
+    printf("Looks good to me.\n");
 }
